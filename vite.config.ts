c028c44190e35a9c5d0fddf27bc1fc00a586529d
watashi <hypereteddy70@gmail.com>
2025-06-04 21:07:34 -0700

    add linting polished

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,17 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          utils: ['./src/utils/zohoApi.ts']
+        }
+      }
+    }
+  },
+  base: './'
 })
